5e96527d6b334c7310f2497014f83f16a002e7d7
Nicolas W <nwadoux@myges.fr>
2024-01-06 16:56:46 +0100

    fix import

--- app/resources/components/AudioSearch.tsx
+++ app/resources/components/AudioSearch.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import KeyboardVoiceRoundedIcon from '@mui/icons-material/KeyboardVoiceRounded';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import { useRouter } from "next/navigation";
 import MicOffIcon from '@mui/icons-material/MicOff';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 export default function AudioSearch({
     onTranscriptChange = (transcript: string) => { },
